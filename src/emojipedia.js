94ab78573c5f6b9166ef270e3ce725800da2817e
Shreya Srivastava <Shreyasrivastava456@gmail.com>
2022-10-30 18:04:02 +0530

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,47 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+      id: 4,
+      emoji: "❤️",
+      name: "Heart",
+      meaning:
+        "While some emojis are very hard to define, this one is rather simple. And that’s because ❤️ is a symbol of love, passion and an expression of feelings. You can use the ❤️ emoji when you love someone or they show you things that you enjoy and appreciate quite a bit."
+    },
+    {
+      id: 5,
+      emoji: "🤓",
+      name: "Nerd Emoji",
+      meaning:
+        "The image of a wide smiling face wearing glasses is the emoji for nerds or studious individuals. It is usually used ironically or in a funny manner to express that a person is being a geek or is trying to show off how smart he or she is. Nerd Emoji can mean “I know what I’m saying because I am so smart!”."
+    },
+    {
+      id: 6,
+      emoji: "😘",
+      name: "Face Blowing a Kiss",
+      meaning:
+        "A yellow face winking with puckered lips blowing a kiss, depicted as a small, red heart. May represent a kiss goodbye or good night and convey feelings of love and affection more generally. "
+    }
+  ];
+  
+  export default emojipedia;
+  
